10449784294e77f014617cd828c7c63cdb22dcb7
Tubaki120507 <rione480@gmail.com>
2024-09-19 05:18:28 +0000

    9-19 JavaScriptの導入

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector("h1").textContent="rione"
+//documentとは
+/*htmlファイルで書かれてるコードを一つのオブジェクトとしてみなす*/
+
+//DOM(ドム)
+/*webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意
+// document.querySelector("h1").textContent="自分の名前"
+
+//ドキュメントのなかの("h1")タグを探して
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えて
+//textContent="自分の名前"
