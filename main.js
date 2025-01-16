6c05d62d9fc23efbc3065a5b083974e0afee513f
Tubaki120507 <rione480@gmail.com>
2025-01-16 05:21:06 +0000

    1-16

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-//function ClickCounterGame(){
+
+function ClickCounterGame(){
     let count = 0;
-    const gameContainer = document.getElementById("game-container");
+    gameContainer = document.getElementById("game-container")
 
-    let button1 = document.createElement("button");
-    button1.textContent="+1ボタン";
+let button1 = document.createElement("button");
+button1.textContent = "+1button";
 
-    let counter = document.createElement("p");
+let button2 = document.createElement("button");
+button2.textContent = "+10button";
+
+let button3 = document.createElement("button");
+button3.textContent = "resetbutton";
+
+let counter = document.createElement("p");
+counter.textContent = count;
+
+button1.addEventListener("click",function(){
+    count++;
     counter.textContent = count;
-    
-    button1.addEventListener("click",function(){
-        count++;
-        counter.textContent = count;
+})
 
-    })
+button2.addEventListener("click",function(){
+    count=count + 10;
+    counter.textContent = count;
+})
 
-    gameContainer.appendChild(button1);
-    gameContainer.appendChild(counter);
+counter.addEventListener("click",function(){
+    count=count *0;
+    counter.textContent = count;
+})
 
-//}
+gameContainer.appendChild(button1);
+gameContainer.appendChild(button2);
+gameContainer.appendChild(button3);
+gameContainer.appendChild(counter);
+}
+
+ClickCounterGame();
